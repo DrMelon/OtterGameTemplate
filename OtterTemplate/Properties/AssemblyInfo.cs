3a91e9bac1cff0150797508d6dd685576564b09e
drmel <drmelon.mail@gmail.com>
2017-03-11 14:44:57 +0000

    Fixed namespacing

--- OtterTemplate/Properties/AssemblyInfo.cs
+++ OtterTemplate/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NotSonic")]
+[assembly: AssemblyTitle("OtterTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NotSonic")]
+[assembly: AssemblyProduct("OtterTemplate")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
